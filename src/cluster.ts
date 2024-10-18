d641cba5174faccb6872a10f4d1daaec0ebed12c
Max <KontinentKM@gmail.com>
2024-10-18 10:00:06 +0300

    feat: scaling

---
+++ src/cluster.ts
@@ -0,0 +1,13 @@
+import cluster from 'cluster';
+import os from 'os';
+import { app } from './index';
+
+const numCPUs = os.cpus().length;
+
+if (cluster.isMaster) {
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker) => {
+    console.log(`Worker ${worker.process.pid}
